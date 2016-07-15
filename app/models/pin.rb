7b240aadd6a379f17fe7fe38301e5af8c626c0db
Jovan Kanev <mrjovan104@gmail.com>
2016-07-15 18:23:59 +0200

    names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ApplicationRecord
   belongs_to :user
   has_attached_file :image, :styles => {:medium => "300x300>",:thumb => "100x100>"}, default_url: "https://unsplash.it/300/200/"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates :description, presence: true
+  validates :image, presence: true
 end
